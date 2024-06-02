3c142692d093c38df24fbff45a38367a861aa544
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-02 15:00:34 +0200

    Added StampaSaluto.java

---
+++ src/main/java/org/lessons/java/StampaSaluto.java
@@ -0,0 +1,12 @@
+package org.lessons.java;
+
+public class StampaSaluto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.printf("Hello World!");
+
+	}
+
+}
